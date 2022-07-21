6fa8859a6426a080a615c71598ee3b207935f41d
Mateus Delai <mateus25delai@gmail.com>
2022-07-21 15:12:43 -0300

    Lista 04 Terminada

---
+++ Lista 04/exercicio_09/main.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+int main()
+{
+    setlocale(LC_ALL, "Portuguese");
+    int vetorA[10], vetorB[10], acumuladora = 0;
+    int j = 0;
+    for(int i = 0; i < 10; i++)
+    {
+        printf("Insira o valor do vetor A [%d]: \n", i);
+        scanf("%d", &vetorA[i]);
+    }
+    for(int i=0; i < 10; i++)
+    {
+        if(vetorA[i] > 0)
+        {
+            vetorB[j] = vetorA[i];
+            acumuladora += 1;
+            j++;
+        }
+    }
+
+
+
+    for(int j = 0; j < acumuladora; j++)
+    {
+        printf("VetorB[%d]= %d. \n", j, vetorB[j]);
+    }
+
+    return 0;
+}
